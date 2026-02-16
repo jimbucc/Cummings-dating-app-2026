d57ffabf5943ad24af64e00179c5684146c146ed
jimbucc <James.Bucciferro@PERKINELMER.COM>
2026-02-16 07:21:14 -0500

    End of Section 5

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -2,33 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   // Inject HttpClient into the component
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = 'Dating App';
-  protected members =  signal<any>([]);
+  protected members =  signal<User[]>([]);
   
   async ngOnInit() {
-    // returns an Observable
-    // this.http.get("https://localhost:5001/api/members")
-    //   .subscribe({  // need to subscribe because of async nature of returned Promise
-    //     next: response => this.members.set(response),
-    //     error: error => console.log(error),
-    //     complete: () => console.log('Completed the http request') // automatically unsubscribed for http request
-    //   })
     this.members.set(await this.getMembers())
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem("user");
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user)
   }
 
   async getMembers() {
-    try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));     
+    try {         
+      return await lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));  
     } catch (error) {
       console.log(error)
       throw error;
