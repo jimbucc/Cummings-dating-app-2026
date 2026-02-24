aae08b6b7970a539989e6e36d56aa4d72e382b39
jimbucc <James.Bucciferro@PERKINELMER.COM>
2026-02-24 16:00:36 -0500

    End of Section 13

--- DatingApp/API/Data/AppDbContext.cs
+++ DatingApp/API/Data/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data;
 
@@ -12,4 +13,26 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }  // represents table name in db
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // call the base
+        base.OnModelCreating(modelBuilder);
+
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+        );
+
+        foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach(var property in entityType.GetProperties())
+            {
+                if(property.ClrType == typeof(DateTime))
+                {
+                    property.SetValueConverter(dateTimeConverter);
+                }
+            }
+        }
+    }
 }
--- DatingApp/API/Data/MemberRepository.cs
+++ DatingApp/API/Data/MemberRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,9 +21,34 @@ public class MemberRepository(AppDbContext context) : IMemberRepository
             .SingleOrDefaultAsync(x => x.Id == id);
     }
 
-    public async Task<IReadOnlyList<Member>> GetMembersAsync()
+    public async Task<PaginatedResult<Member>> GetMembersAsync(MemberParams memberParams)
     {
-        return await context.Members.ToListAsync();
+        var query = context.Members.AsQueryable();
+
+        // Filter out the current member
+        query = query.Where(x => x.Id != memberParams.CurrentMemberId);
+
+        // Filter by gender if requested
+        if(memberParams.Gender != null)
+        {
+            query = query.Where(x => x.Gender == memberParams.Gender);
+        }
+
+        // get the minimum and maximum age
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-memberParams.MinAge));
+
+        query = query.Where(x => x.DateOfBirth >= minDob && x.DateOfBirth <= maxDob);
+
+        // sorting by created date or last active date
+        query = memberParams.OrderBy switch
+        {
+          "created" => query.OrderByDescending(x => x.Created)  ,
+          _ => query.OrderByDescending(x => x.LastActive)
+        };
+
+        return await PaginationHelper
+            .CreateAsync(query, memberParams.PageNumber, memberParams.PageSize);        
     }
 
     public async Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId)
